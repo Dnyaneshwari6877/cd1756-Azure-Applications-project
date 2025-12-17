8b691442bb8dbbda895269abcf75b4283119d249
Dnyaneshwari6877 <110388060+Dnyaneshwari6877@users.noreply.github.com>
2025-12-17 19:28:50 +0530

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,6 +10,10 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Ensure Flask correctly detects HTTPS when behind Azure App Service
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
 # TODO: Add any logging levels and handlers with app.logger
 Session(app)
 db = SQLAlchemy(app)
