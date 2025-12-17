b225364fc18512de45453a1d4c69129ce7d82a4c
Dnyaneshwari6877 <110388060+Dnyaneshwari6877@users.noreply.github.com>
2025-12-17 20:35:15 +0530

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -118,7 +118,7 @@ def logout():
         # Also logout from your tenant's web session
         return redirect(
             Config.AUTHORITY + "/oauth2/v2.0/logout" +
-            "?post_logout_redirect_uri=" + url_for("login", _external=True))
+            "?post_logout_redirect_uri=" + url_for("login", _external=True,_scheme="https"))
 
     return redirect(url_for('login'))
 
@@ -144,5 +144,5 @@ def _build_auth_url(authority=None, scopes=None, state=None):
     return _build_msal_app(authority=authority).get_authorization_request_url(
         scopes or [],
         state=state,
-        redirect_uri=url_for("authorized", _external=True)
+        redirect_uri=url_for("authorized", _external=True,_scheme="https")
     )
